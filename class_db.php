42d67c0e8ed8a6d3e5d06795f4a02961bab424a9
linux-days <linuxdays2015@gmail.com>
2017-12-18 18:22:06 +0900

    Update class_db.php

--- class_db.php
+++ class_db.php
@@ -1,6 +1,6 @@
 <?php
 /*
-* Filename.......: class_db.php Master
+* Filename.......: class_db.php Master wrapper
 * Author.........: Troy Wolf [troy@troywolf.com]
 * Last Modified..: Date: 2005/06/24 11:05:00
 * Description....: A database class that provides methods to work with mysql,
